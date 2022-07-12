60a9a6cec592a772a5a256131c9f9d87dc57ecf8
Chase Norman <c_@berkeley.edu>
2022-07-12 01:28:11 -0400

    Initial Commit

--- lichess.py
+++ lichess.py
@@ -2,9 +2,12 @@ import requests
 import chess
 import urllib
 import json
+import time
+import random
 
 cache_file = 'cache.json'
 cache = json.load(open(cache_file))
+rate_limit_sleep = 1
 
 def flush_cache():
     with open(cache_file, 'w') as fp:
@@ -13,22 +16,26 @@ def flush_cache():
 def explorer(board):
     fen = urllib.parse.quote(board.fen(), safe='')
     if fen not in cache or "explorer" not in cache[fen]:
+        time.sleep(rate_limit_sleep)
         request = requests.get(f'https://explorer.lichess.ovh/lichess?variant=standard&speeds=rapid&ratings=1600&topGames=0&recentGames=0&fen={fen}')
         if fen not in cache:
             cache[fen] = dict()
         cache[fen]["explorer"] = json.loads(request.text)
     result = cache[fen]["explorer"]
     total = result["white"] + result["draws"] + result["black"]
-    e = []
+    moves = []
+    weights = []
     for move in result["moves"]:
         count = move["white"] + move["draws"] + move["black"]
-        e.append((count/total, chess.Move.from_uci(move["uci"])))
-    return e
+        weights.append(count/total)
+        moves.append(chess.Move.from_uci(move["uci"]))
+    return moves, weights
 
 
 def cloud_eval(board):
     fen = urllib.parse.quote(board.fen(), safe='')
     if fen not in cache or "cloud_eval" not in cache[fen]:
+        time.sleep(rate_limit_sleep)
         request = requests.get(f'https://lichess.org/api/cloud-eval?fen={fen}')
         if fen not in cache:
             cache[fen] = dict()
@@ -37,3 +44,17 @@ def cloud_eval(board):
     if "error" in result:
         return None
     return chess.Move.from_uci(result["pvs"][0]["moves"].split()[0]), result["pvs"][0]["cp"] / 100
+
+
+def pick_position():
+    board = chess.Board()
+    color = bool(random.getrandbits(1))
+    while True:
+        if color == board.turn:
+            best_move = cloud_eval(board)[0]
+            if bool(random.getrandbits(1)):
+                return board, best_move
+            board.push(best_move)
+        else:
+            moves, weights = explorer(board)
+            board.push(random.choices(moves, weights=weights, k=1)[0])
--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from lichess import *
 
 board_size = 500
 
-flipped = False
 
 from_square = None
 to_square = None
@@ -17,30 +16,48 @@ window = pygame.display.set_mode((board_size, board_size))
 clock = pygame.time.Clock()
 
 
-board = chess.Board()
-cloud_eval(board)
+board, best_move = pick_position()
+flipped = not board.turn
 
-def render():
+incorrect = False
+correct_timer = 0
+
+def render(fill=None, lastmove=None):
+    if fill is None:
+        fill = dict()
     global pygame_surface
     svg_string = chess.svg.board(
         board=board,
         size=board_size,
         coordinates=False,
-        flipped=flipped
+        flipped=flipped,
+        fill=fill,
+        lastmove=lastmove
     )
     pygame_surface = pygame.image.load(io.BytesIO(svg_string.encode()))
 
 
+def last_move():
+    try:
+        return board.peek()
+    except IndexError:
+        return None
+
 pygame_surface = None
-render()
+render(lastmove=last_move())
 
 def try_move():
-    global from_square, to_square
+    global from_square, to_square, correct_timer, incorrect
     try:
         move = board.find_move(from_square, to_square)
-        print(move)
-        board.push(move)
-        render()
+        if move == best_move:
+            board.push(move)
+            render(fill={to_square: "#acce5980", from_square: "#acce5980"})
+            correct_timer = 10
+        else:
+            board.push(move)
+            incorrect = True
+            render(fill={to_square: "#c9343080", from_square: "#c9343080"})
         from_square = None
         to_square = None
     except ValueError:
@@ -54,10 +71,10 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and not incorrect:
             file, rank = map(lambda a: math.floor(a*8/board_size), pygame.mouse.get_pos())
             pos = rank*8 + (7 - file) if flipped else (7-rank)*8 + file
-            if from_square == None:
+            if from_square is None:
                 from_square = pos
             else:
                 to_square = pos
@@ -65,13 +82,25 @@ while run:
         if event.type == pygame.MOUSEBUTTONUP:
             file, rank = map(lambda a: math.floor(a * 8 / board_size), pygame.mouse.get_pos())
             pos = rank*8 + (7 - file) if flipped else (7 - rank) * 8 + file
-            if from_square and pos != from_square:
+            if incorrect and pos != from_square:
+                board.pop()
+                render(lastmove=last_move())
+                incorrect = False
+            elif from_square and pos != from_square:
                 to_square = pos
                 try_move()
 
 
     window.fill((255, 255, 255))
-    window.blit(pygame_surface, pygame_surface.get_rect(center = window.get_rect().center))
+    window.blit(pygame_surface, pygame_surface.get_rect(center=window.get_rect().center))
+    if correct_timer:
+        correct_timer -= 1
+        if not correct_timer:
+            board, best_move = pick_position()
+            to_square = None
+            from_square = None
+            flipped = not board.turn
+            render(lastmove=last_move())
     pygame.display.flip()
 
 
